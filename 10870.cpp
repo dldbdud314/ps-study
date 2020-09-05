7a723546bbf6483fe43894aed92203270fd1b18e
dldbdud314 <dldbdud314@naver.com>
2020-09-05 17:09:22 +0900

    재귀 알고리즘

---
+++ 10870.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int a){
+    if(a==0) return 0;
+    else if(a==1) return 1;
+    else return fibonacci(a-2)+fibonacci(a-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+
+    return 0;
+}
