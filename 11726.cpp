ed5111f101105f13f8a41fe471002fc94e6aa649
dldbdud314 <dldbdud314@naver.com>
2021-03-30 15:48:55 +0900

    DP

---
+++ 11726.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int tiles[1001];
+
+int tiling(int n){
+    if(tiles[n]) return tiles[n];
+    tiles[n] = tiling(n-1) + tiling(n-2);
+    
+    return tiles[n]%10007;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    tiles[1] = 1;
+    tiles[2] = 2;
+    
+    cout << tiling(n);
+}
